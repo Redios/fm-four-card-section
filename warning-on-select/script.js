3c2c220a296431d48a1ba190c66f59cae2062483
Serj <redska@gmail.com>
2024-02-13 23:33:16 +0300

    Create script.js

---
+++ warning-on-select/script.js
@@ -0,0 +1,7 @@
+const dropdown = document.getElementById("form-field-xxjupb");
+
+dropdown.onchange = function(event) {
+    if (dropdown.value=="I did not") {
+        alert("A quick review on Amazon means the world to us! You can leave a short review and access your bonuses.")
+    }
+}
