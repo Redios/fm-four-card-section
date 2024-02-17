6e860aee86ba3f878a963e694a3dac60e8052c27
Serj <redska@gmail.com>
2024-02-17 22:53:07 +0300

    Update script.js

--- warning-on-select/script.js
+++ warning-on-select/script.js
@@ -1,7 +1,14 @@
-const dropdown = document.getElementById("form-field-xxjupb");
+<script>
+/* used for displaying a message to a user that selects the wrong option and hides a submit button */
+const dropdown = document.querySelector('[name="form-field-xxjupb"]');
 
 dropdown.onchange = function(event) {
     if (dropdown.value=="I did not") {
         alert("A quick review on Amazon means the world to us! You can leave a short review and access your bonuses.")
+      document.querySelector('[type="submit"]').style.display = 'none';
+    } else {
+      document.querySelector('[type="submit"]').style.display = 'inline-flex';
     }
 }
+
+</script>
